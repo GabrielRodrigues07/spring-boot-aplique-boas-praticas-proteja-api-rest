231791f46a65d443c568ae4f7b47c5472c7fb47e
GabrielRodrigues07 <grodrigues256@gmail.com>
2023-01-25 09:54:51 -0300

    Refatorando codigo usando o ResponseEntity

--- src/main/java/med/voll/api/controller/MedicoController.java
+++ src/main/java/med/voll/api/controller/MedicoController.java
@@ -5,13 +5,16 @@ import med.voll.api.assembler.MedicoAssembler;
 import med.voll.api.dto.AtualizacaoMedicoDTO;
 import med.voll.api.dto.MedicoDTO;
 import med.voll.api.dto.MedicoResumoDTO;
+import med.voll.api.medico.DadosDetalhamentoMedico;
 import med.voll.api.medico.Medico;
 import med.voll.api.medico.MedicoRepository;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.web.PageableDefault;
+import org.springframework.http.ResponseEntity;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.util.UriComponentsBuilder;
 
 import javax.validation.Valid;
 
@@ -26,28 +29,32 @@ public class MedicoController {
 
     @PostMapping
     @Transactional
-    public void cadastrar(@RequestBody @Valid MedicoDTO medicoDTO) {
+    public ResponseEntity cadastrar(@RequestBody @Valid MedicoDTO medicoDTO, UriComponentsBuilder uriBuilder) {
         Medico medico = medicoAssembler.toEntity(medicoDTO);
         medico.setAtivo(true);
         repository.save(medico);
+        var uri = uriBuilder.path("/medicos/{id}").buildAndExpand(medico.getId()).toUri();
+        return ResponseEntity.created(uri).body(new DadosDetalhamentoMedico(medico));
     }
 
     @GetMapping
-    public Page<MedicoResumoDTO> listar(@PageableDefault(size = 10, sort = {"nome"}) Pageable paginacao) {
-        return repository.findAllByAtivoTrue(paginacao).map(medico -> medicoAssembler.toResumoDTO(medico));
+    public ResponseEntity<Page<MedicoResumoDTO>> listar(@PageableDefault(size = 10, sort = {"nome"}) Pageable paginacao) {
+        return ResponseEntity.ok().body(repository.findAllByAtivoTrue(paginacao).map(medico -> medicoAssembler.toResumoDTO(medico)));
     }
 
     @PutMapping
     @Transactional
-    public void atualizar(@RequestBody @Valid AtualizacaoMedicoDTO atualizacaoMedicoDTO) {
+    public ResponseEntity atualizar(@RequestBody @Valid AtualizacaoMedicoDTO atualizacaoMedicoDTO) {
         var medico = repository.getReferenceById(atualizacaoMedicoDTO.getId());
         medico.atualizarInformacoes(atualizacaoMedicoDTO);
+        return ResponseEntity.ok(new DadosDetalhamentoMedico(medico));
     }
 
     @DeleteMapping(value = "/{idMedico}")
     @Transactional
-    public void excluir(@PathVariable Long idMedico) {
+    public ResponseEntity excluir(@PathVariable Long idMedico) {
         var medico = repository.getReferenceById(idMedico);
         medico.excluir();
+        return ResponseEntity.noContent().build();
     }
 }
