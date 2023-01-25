7f6e8d271bc679ad8c4944760f957abab381d9e9
GabrielRodrigues07 <grodrigues256@gmail.com>
2023-01-25 10:25:08 -0300

    Refatorando codigo usando ResponseEntity

--- src/main/java/med/voll/api/controller/PacienteController.java
+++ src/main/java/med/voll/api/controller/PacienteController.java
@@ -5,13 +5,16 @@ import med.voll.api.assembler.PacienteAssembler;
 import med.voll.api.dto.AtualizacaoPacienteDTO;
 import med.voll.api.dto.PacienteDTO;
 import med.voll.api.dto.PacienteResumoDTO;
+import med.voll.api.paciente.DadosDetalhamentoPaciente;
 import med.voll.api.paciente.Paciente;
 import med.voll.api.paciente.PacienteRepository;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.web.PageableDefault;
+import org.springframework.http.ResponseEntity;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.util.UriComponentsBuilder;
 
 import javax.validation.Valid;
 
@@ -25,27 +28,32 @@ public class PacienteController {
 
     @PostMapping
     @Transactional
-    public void cadastrar(@RequestBody @Valid PacienteDTO pacienteDTO) {
+    public ResponseEntity cadastrar(@RequestBody @Valid PacienteDTO pacienteDTO, UriComponentsBuilder uriBuilder) {
         Paciente paciente = pacienteAssembler.toEntity(pacienteDTO);
         repository.save(paciente);
+        var uri = uriBuilder.path("/pacientes/{id}").buildAndExpand(paciente.getId()).toUri();
+        return ResponseEntity.created(uri).body(new DadosDetalhamentoPaciente(paciente));
     }
 
     @GetMapping
-    public Page<PacienteResumoDTO> listar(@PageableDefault(size = 10, sort = {"nome"}) Pageable paginacao) {
-        return repository.findAllByAtivoTrue(paginacao).map(paciente -> pacienteAssembler.toResumoDTO(paciente));
+    public ResponseEntity<Page<PacienteResumoDTO>> listar(@PageableDefault(size = 10, sort = {"nome"}) Pageable paginacao) {
+        Page<PacienteResumoDTO> pacientes = repository.findAllByAtivoTrue(paginacao).map(paciente -> pacienteAssembler.toResumoDTO(paciente));
+        return ResponseEntity.ok(pacientes);
     }
 
     @PutMapping
     @Transactional
-    public void atualizar(@RequestBody @Valid AtualizacaoPacienteDTO atualizacaoPacienteDTO) {
+    public ResponseEntity atualizar(@RequestBody @Valid AtualizacaoPacienteDTO atualizacaoPacienteDTO) {
         var paciente = repository.getReferenceById(atualizacaoPacienteDTO.getId());
         paciente.atualizarInformacoes(atualizacaoPacienteDTO);
+        return ResponseEntity.ok(new DadosDetalhamentoPaciente(paciente));
     }
 
     @DeleteMapping(value = "/{idPaciente}")
     @Transactional
-    public void excluir(@PathVariable Long idPaciente) {
+    public ResponseEntity excluir(@PathVariable Long idPaciente) {
         var paciente = repository.getReferenceById(idPaciente);
         paciente.excluir();
+        return ResponseEntity.noContent().build();
     }
 }
---
+++ src/main/java/med/voll/api/paciente/DadosDetalhamentoPaciente.java
@@ -0,0 +1,26 @@
+package med.voll.api.paciente;
+
+import lombok.Getter;
+import lombok.Setter;
+import med.voll.api.endereco.Endereco;
+
+@Getter @Setter
+public class DadosDetalhamentoPaciente {
+
+    private Long id;
+    private String nome;
+    private String email;
+    private String cpf;
+    private String telefone;
+    private Endereco endereco;
+
+    public DadosDetalhamentoPaciente(Paciente paciente) {
+
+        this.id = paciente.getId();
+        this.nome = paciente.getNome();
+        this.email = paciente.getEmail();
+        this.cpf = paciente.getCpf();
+        this.telefone = paciente.getTelefone();
+        this.endereco = paciente.getEndereco();
+    }
+}
