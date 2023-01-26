16cd2f1c403f7541d004a601fdd092600126c72b
GabrielRodrigues07 <grodrigues256@gmail.com>
2023-01-26 08:35:35 -0300

    tratando erro 400 no controller de medico

--- src/main/java/med/voll/api/infra/TratadorDeErros.java
+++ src/main/java/med/voll/api/infra/TratadorDeErros.java
@@ -1,9 +1,16 @@
 package med.voll.api.infra;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
 import med.voll.api.controller.exception.ObjectNotFoundException;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.List;
 
 @RestControllerAdvice
 public class TratadorDeErros {
@@ -12,4 +19,25 @@ public class TratadorDeErros {
     public ResponseEntity tratarErro404() {
         return ResponseEntity.notFound().build();
     }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity tratarErro400(MethodArgumentNotValidException ex) {
+        List<FieldError> fieldErrors = ex.getFieldErrors();
+        return ResponseEntity.badRequest().body(fieldErrors
+                .stream()
+                .map(DadosErroValidacao::new));
+    }
+
+
+    @Getter @Setter
+    @AllArgsConstructor
+    private final class DadosErroValidacao {
+        private String campo;
+        private String mensagem;
+
+        public DadosErroValidacao(FieldError error) {
+            this(error.getField(), error.getDefaultMessage());
+        }
+    }
 }
+
