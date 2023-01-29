e94eb0367720c1c849c35a85229281597d276a62
GabrielRodrigues07 <grodrigues256@gmail.com>
2023-01-29 09:00:58 -0300

    Recuperando Token

--- src/main/java/med/voll/api/infra/security/SecurityFilter.java
+++ src/main/java/med/voll/api/infra/security/SecurityFilter.java
@@ -13,6 +13,19 @@ import java.io.IOException;
 public class SecurityFilter extends OncePerRequestFilter {
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+
+        var tokenJWT = recuperarToken(request);
+
+        System.out.println(tokenJWT);
+
         filterChain.doFilter(request, response);
+    }
+
+    private String recuperarToken(HttpServletRequest request) {
+        var authorizationHeader = request.getHeader("Authorization");
+        if (authorizationHeader == null) {
+            throw new RuntimeException("TokenJWT não enviado no cabeçalho Authorization!");
+        }
+        return authorizationHeader.replace("Bearer ", "");
     }
 }
